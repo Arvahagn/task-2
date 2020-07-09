cb0a6351c72f7728435bdd443b4163afcd39955f
Arvahagn <Arvahagn@users.noreply.github.com>
2020-07-09 13:14:15 +0400

    task

---
+++ task2.js
@@ -0,0 +1,29 @@
+
+let a = 32;
+let b = 21;
+
+// 1.
+let sum = a + b;
+console.log(sum);
+alert(sum);
+
+// 2. 
+let sub = a - b;
+console.log(sub);
+alert(sub);
+
+// 3. 
+let mult = a * b;
+console.log(mult);
+alert(mult);
+
+// 4.
+let divs = a / b;
+console.log(divs);
+alert(divs);
+
+// 5. 
+let rem = a % b;
+console.log(rem);
+alert(rem);
+
